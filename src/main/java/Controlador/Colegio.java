c0eb55f9b14e7c0f8774274994bd4fedcdeed826
veronz07 <veronica.nunez@uao.edu.co>
2025-10-19 16:53:30 -0500

    Se crea la clase Colegio en el paquete controlador con listas para profesores y estudiantes.

---
+++ src/main/java/Controlador/Colegio.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import Modelo.*;
+
+/**
+ *
+ * @author Vero
+ */
+public class Colegio {
+    private ArrayList<Profesor> Profesores;
+    private ArrayList<Estudiante> Estudiantes;
+    
+    public Colegio(){
+        Profesores= new ArrayList<>();
+        Estudiantes= new ArrayList<>();
+    }
+    
+    
+}
