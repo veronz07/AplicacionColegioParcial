32b5c308056e6c53038f2a7abd1067fbf6c36db7
veronz07 <veronica.nunez@uao.edu.co>
2025-10-19 17:34:34 -0500

    Creación del método registrarProfesor en donde se añade la información de un profesor y se crea el objeto.

--- src/main/java/Controlador/Colegio.java
+++ src/main/java/Controlador/Colegio.java
@@ -12,13 +12,27 @@ import Modelo.*;
  * @author Vero
  */
 public class Colegio {
-    private ArrayList<Profesor> Profesores;
-    private ArrayList<Estudiante> Estudiantes;
+    private ArrayList<Profesor> profesores;
+    private ArrayList<Estudiante> estudiantes;
     
     public Colegio(){
-        Profesores= new ArrayList<>();
-        Estudiantes= new ArrayList<>();
+        profesores= new ArrayList<>();
+        estudiantes= new ArrayList<>();
     }
-    
-    
+    public void registrarProesor(){
+        String nombre = JOptionPane.showInputDialog("Ingrese el nombre del profesor: ");
+        String direccion= JOptionPane.showInputDialog("Ingrese la dirección de residencia: ");
+        String telefono = JOptionPane.showInputDialog("Ingrese el número telefónico: ");
+        String fechaNacimiento= JOptionPane.showInputDialog("Ingrese la fecha de nacimiento: ");
+        String cedula= JOptionPane.showInputDialog("Ingrese la cédula: ");
+        String area = JOptionPane.showInputDialog("Ingrese la materia que enseña");
+        double salarioHora = Double.parseDouble(JOptionPane.showInputDialog("Ingrese su salario por hora: "));
+        double horasTrabajadas = Double.parseDouble(JOptionPane.showInputDialog("Ingrese las horas trabajadas: "));
+        
+        Profesor p = new Profesor(nombre, direccion, telefono, fechaNacimiento, cedula, area, salarioHora, horasTrabajadas);
+        
+        profesores.add(p);
+        
+        JOptionPane.showMessageDialog(null,"Profesor registrado exitosamente.");
+    } 
 }
