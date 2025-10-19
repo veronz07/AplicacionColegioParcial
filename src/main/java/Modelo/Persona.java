50d8426cc5940607bc05156d2cfc338f1b47af66
veronz07 <veronica.nunez@uao.edu.co>
2025-10-19 13:30:06 -0500

    Se crea la clase persona con sus respectivos constructores, uno vacío para que no hayan errores cuando se intenta ver la lista sin ningún dato ingresado y otro en donde están todos los atributos.

---
+++ src/main/java/Modelo/Persona.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Vero
+ */
+public class Persona {
+    protected String nombre;
+    protected String direccion;
+    protected String telefono;
+    protected String fechaNacimiento;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String direccion, String telefono, String fechaNacimiento) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+}
