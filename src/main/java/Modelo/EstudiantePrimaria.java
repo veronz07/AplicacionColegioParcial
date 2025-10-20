ecc632378cc9ea299c2562b523314bcd4973fb5f
veronz07 <veronica.nunez@uao.edu.co>
2025-10-19 21:30:19 -0500

    cambio de String a boolean en programasApoyo

--- src/main/java/Modelo/EstudiantePrimaria.java
+++ src/main/java/Modelo/EstudiantePrimaria.java
@@ -9,34 +9,32 @@ package Modelo;
  * @author Vero
  */
 public class EstudiantePrimaria extends Estudiante {
-    private String programasApoyo;
+    private boolean programasApoyo;  // ← cambia de String a boolean
     private String nivelComprensionLectora;
 
-    public EstudiantePrimaria() {
-    }
-    
+    public EstudiantePrimaria() {}
 
     public EstudiantePrimaria(String nombre, String direccion, String telefono, String fechaNacimiento,
-                              int codigo, int grado, String programasApoyo, String nivelComprensionLectora) {
+                              int codigo, int grado, boolean programasApoyo, String nivelComprensionLectora) {
         super(nombre, direccion, telefono, fechaNacimiento, codigo, grado);
         this.programasApoyo = programasApoyo;
         this.nivelComprensionLectora = nivelComprensionLectora;
     }
 
-    public String getProgramasApoyo() {
+    public boolean tieneProgramasApoyo() {
         return programasApoyo;
     }
 
     public String getNivelComprensionLectora() {
         return nivelComprensionLectora;
     }
-    
+
     @Override
     public String toString() {
         return super.toString() +
-               ", Programas de apoyo: " + programasApoyo +
+               ", Programas de apoyo: " + (programasApoyo ? "Sí" : "No") +
                ", Nivel de comprensión lectora: " + nivelComprensionLectora;
     }
-
 }
+
 
