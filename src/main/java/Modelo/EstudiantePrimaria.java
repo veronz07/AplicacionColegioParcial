1c9070b8bee2510dcb81e172e10e0c23963702ea
veronz07 <veronica.nunez@uao.edu.co>
2025-10-19 20:16:27 -0500

    Se crea la clase EstudiantePrimaria que hereda información de la clase Estudiante con sus respectivos constructores

---
+++ src/main/java/Modelo/EstudiantePrimaria.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Vero
+ */
+public class EstudiantePrimaria extends Estudiante {
+    private String programasApoyo;
+    private String nivelComprensionLectora;
+
+    public EstudiantePrimaria(String nombre, String direccion, String telefono, String fechaNacimiento,
+                              int codigo, int grado, String programasApoyo, String nivelComprensionLectora) {
+        super(nombre, direccion, telefono, fechaNacimiento, codigo, grado);
+        this.programasApoyo = programasApoyo;
+        this.nivelComprensionLectora = nivelComprensionLectora;
+    }
+
+    public String getProgramasApoyo() {
+        return programasApoyo;
+    }
+
+    public String getNivelComprensionLectora() {
+        return nivelComprensionLectora;
+    }
+
+}
+
