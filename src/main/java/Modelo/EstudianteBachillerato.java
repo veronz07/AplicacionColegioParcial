3eb8c6a1f102065d7302f64a835982e2a46c799d
veronz07 <veronica.nunez@uao.edu.co>
2025-10-19 20:10:09 -0500

    Creación de la clase EstudianteBachillerato que hereda información de la clase Estudiante con sus respectivos constructores

---
+++ src/main/java/Modelo/EstudianteBachillerato.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Vero
+ */
+public class EstudianteBachillerato extends Estudiante{
+    private String nivelDeIgles;
+    private double horasServicioSocial;
+    public EstudianteBachillerato(String nombre, String direccion, String telefono, String fechaNacimiento,
+                                  int codigo, int grado, double horasServicioSocial, String nivelDeIngles) {
+        super(nombre, direccion, telefono, fechaNacimiento, codigo, grado);
+        this.horasServicioSocial = horasServicioSocial;
+        this.nivelDeIgles = nivelDeIngles;
+    }
+
+    public double getHorasServicioSocial() {
+        return horasServicioSocial;
+    }
+
+    public String getNivelIngles() {
+        return nivelDeIgles;
+    }    
+}
